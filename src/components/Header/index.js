472384c41d02d88865c718a755ffb41cb4004034
Glitch (bai26-reactrouter-reactjs) <none>
2020-06-05 05:21:23 +0000

    😼🚀 Updated with Glitch

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import Logo from '../Logo/index';
+import MainMenu from '../Logo/MainMenu';
+import 'style.css'
+
+
+export default class Header extends Component {
+  
+}
